95a01d0758f9d25f0e216c21b5acaf45155257b8
Leonardo Stabile <stabileleonardo@hotmail.com>
2018-07-09 13:09:59 -0300

    Concluido layout de tela de historico de partidas

---
+++ app/src/main/java/tatahcorte/jogodaspalavras/view/ActivityHistorico.java
@@ -0,0 +1,15 @@
+package tatahcorte.jogodaspalavras.view;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import tatahcorte.jogodaspalavras.R;
+
+public class ActivityHistorico extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_historico);
+    }
+}
